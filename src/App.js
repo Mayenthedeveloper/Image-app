ff4f3ef943f8d180ed30c317cf8bba2b9ef8e74b
Mayen Akpan <neroxma@yahoo.com>
2021-09-01 17:00:06 -0700

    Added APIKEY

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import "./App.css";
+import Hero from "./components/Hero";
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const API_KEY = "23182425-11dd7e2a724aa85d4a73cb277";
+
+  const [images, setImages] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [search, setSearch] = useState("");
+  const [inputVal, setInputVal] = useState("");
+
+  useEffect(() => {
+    fetch(
+      `https://pixabay.com/api/?key=${API_KEY}&q=${search}&image_type=photo&per_page=9&page=${currentPage}&pretty=true`
+    )
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  }, []);
+
   return (
     <div className="App">
-      <h1>Image App</h1>
+      <Hero />
     </div>
   );
 }
