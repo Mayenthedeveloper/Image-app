04b4e139eef9eaf6a2d2cc69f80b94cb6fb724f7
Mayen Akpan <neroxma@yahoo.com>
2021-09-03 21:00:19 -0700

    Updated

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,57 @@
 import React from "react";
 
-export default function Cart({ images }) {
+const Card = ({ images }) => {
   return (
     <div>
-      <>{images.map((image) => console.log(image.tags.split(",")))}</>
+      <>
+        {images.map((image) => (
+          <div className="card" key={image.id}>
+            <a
+              href={image.largeImageURL}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                src={image.webformatURL}
+                alt="photograph"
+                className="cardImage"
+              />
+              <div className="cardInfo">
+                <p className="user">Photo by {image.user} </p>
+                <p>
+                  <i className="fas fa-eye"></i>
+                  <span>Views: </span>
+                  {image.views}
+                </p>
+                <p>
+                  <i className="fas fa-downloads"></i>
+                  <span>Downloads:</span>
+                  {image.downloads}
+                </p>
+                <p>
+                  <i className="fas fa-heart"></i>
+                  <span>Likes:</span>
+                  {image.likes}
+                </p>
+                <p>
+                  <i className="fas fa-comment"></i>
+                  <span>Comments:</span>
+                  {image.comments}
+                </p>
+                <div className="tags">
+                  {image.tags
+                    .split(",")
+                    .map((tag, index) =>
+                      tag.length >= 11 ? "" : <span key={index}>#{tag}</span>
+                    )}
+                </div>
+              </div>
+            </a>
+          </div>
+        ))}
+      </>
     </div>
   );
-}
+};
+
+export default Card;
